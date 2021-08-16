fe1c7cb1f1e807472b8d707bd8072b73a32c1e6f
Nehakamble <juhikamble2@gmail.com>
2021-08-16 15:44:12 +0530

    integrating eureka with account and loan

---
+++ zuul-gateway/src/main/java/com/fis/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.fis.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+@EnableAspectJAutoProxy 
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+}
